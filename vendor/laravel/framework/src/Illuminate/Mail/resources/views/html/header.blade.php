a1d3f6f9dacf6a3f2881a35d08301b777c7afe76
pancy-dev <pancyboxi@gmail.com>
2020-09-15 10:52:18 +0300

    change email template

--- vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
+++ vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
@@ -1,11 +1,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
+<img src="{{Config::get('app.url')}}assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext">
 </a>
 </td>
 </tr>
+    
