1c2273df06ea6a9a40ff06a7195d59d665d36f35
pancy-dev <pancyboxi@gmail.com>
2020-09-15 22:57:08 +0300

    mailteplate customize

--- config/mail.php
+++ config/mail.php
@@ -40,7 +40,7 @@ return [
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
-            'password' => env('qweQWE123!@#'),
+            'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
         ],
 
