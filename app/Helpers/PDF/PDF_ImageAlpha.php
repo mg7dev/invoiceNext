7b465c881796c9a3e471f6ecb42db12e325114c4
pancy-dev <pancyboxi@gmail.com>
2020-09-15 19:04:12 +0300

    sdfsd

--- app/Helpers/PDF/PDF_ImageAlpha.php
+++ app/Helpers/PDF/PDF_ImageAlpha.php
@@ -94,7 +94,6 @@ function Image($file, $x=null, $y=null, $w=0, $h=0, $type='', $link='', $isMask=
 function ImagePngWithAlpha($file,$x,$y,$w=0,$h=0,$link='')
 {
     $tmp_alpha = tempnam('/var/www/html/storage', 'mska');
-    dd($tmp_alpha);
     $this->tmpFiles[] = $tmp_alpha;
     $tmp_plain = tempnam('/var/www/html/storage', 'mskp');
     $this->tmpFiles[] = $tmp_plain;
