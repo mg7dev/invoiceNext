431e2672521f102a0119f3a972099e9a79ecb6bc
pancy-dev <pancyboxi@gmail.com>
2020-09-16 01:00:46 +0300

    dfs

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="http://165.232.66.234/assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext">
+<img src="http://165.232.66.234/assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext" style="width:50%">
 </a>
 </td>
 </tr>
