42f912dc14b56df7903abe3ffded03891a91a6cb
pancy-dev <pancyboxi@gmail.com>
2020-09-15 13:51:27 +0300

    meta tag fix

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title')</title>
+    <title>@yield('title') - InvoiceNext</title>
 
     @include('layouts._css')
 </head>
