12f93dd83fb5abec0d7273f6f926e535e20100d8
pancy-dev <pancyboxi@gmail.com>
2020-09-16 01:44:25 +0300

    dfs

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,12 +2,13 @@
 <td class="header">
 <a href="{{ $url }}" class="_mail_header_link">
     <img src="http://165.232.66.234/assets/images/favicon.png" class="_mail_header_favicon"  alt="InvoiceNext" >
-    <img src="http://165.232.66.234/assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext" style="width:50%;">
+    <img src="http://165.232.66.234/assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext" style="width:30%;">
 </a>
 </td>
 </tr>
 <style>
 ._mail_header_link{
+    width:100%!important;
     align-items:center;
     background-color:rgba(0, 0, 0, 0);
     box-sizing:border-box;
