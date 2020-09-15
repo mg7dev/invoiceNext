90d1691db3ae4ea411430289853d322ea227bf72
pancy-dev <pancyboxi@gmail.com>
2020-09-15 15:21:45 +0300

    verify page update

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,3 +1,7 @@
+@php
+    $authUser = Auth::User();
+@endphp
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
 
@@ -13,37 +17,36 @@
 
 <body class="layout-default has-drawer-opened">
     <div class="mdk-header-layout js-mdk-header-layout">
-   
+        @include('layouts._header')
 
         <div class="mdk-header-layout__content pt-64px">
             <div class="mdk-drawer-layout js-mdk-drawer-layout">
                 <div class="mdk-drawer-layout__content page">
-                    <div class="container-fluid page__heading-container">
-                        @yield('page_header')
-                    </div>
-
                     <div class="container-fluid page__container">
-                        <div class="container">
-                            <div class="row justify-content-center">
-                                <div class="col-md-8">
-                                    <div class="card">
-                                        <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-                        
-                                        <div class="card-body">
-                                            @if (session('resent'))
-                                                <div class="alert alert-success" role="alert">
-                                                    {{ __('A fresh verification link has been sent to your email address.') }}
-                                                </div>
-                                            @endif
-                        
-                                            {{ __('Before proceeding, please check your email for a verification link.') }}
-                                            {{ __('If you did not receive the email') }},
-                                            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                                                @csrf
-                                                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                                            </form>
+                        <div class="row justify-content-center">
+                            <div class="d-flex flex-column justify-content-center align-items-center mt-2 mb-2 navbar-light">
+                                <a href="{{ url('/login') }}" class="navbar-brand flex-column mb-2 align-items-center mr-0">
+                                    <img class="navbar-brand-icon mr-0 mb-2" src="{{asset('assets/images/favicon.png')}}" width="150" alt="{{ config('app.name') }}">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="d-flex flex-column justify-content-center align-items-center mt-2 mb-2 navbar-light">
+                                <h1 class="text-center h6 mb-4">{{ __('Verify Your Email Address') }}</h1>
+                                <div class="card-body">
+                                    @if (session('resent'))
+                                        <div class="alert alert-success" role="alert">
+                                            {{ __('A fresh verification link has been sent to your email address.') }}
                                         </div>
-                                    </div>
+                                    @endif
+            
+                                    <h5>
+                                        {{ __('Before proceeding, please check your email for a verification link.') }}
+                                    </h5>
+                                    <form method="POST" action="{{ route('verification.resend') }}">
+                                        @csrf
+                                        <button type="submit" class="btn btn-success form-control">{{ __('click here to request another') }}</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
