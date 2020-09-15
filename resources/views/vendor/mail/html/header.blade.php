662a31b380a9370eef9b272398ecc446825a92cf
pancy-dev <pancyboxi@gmail.com>
2020-09-15 22:22:31 +0300

    mailteplate customize

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{Config::get('app.url')}}assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext">
+</a>
+</td>
+</tr>
+    
