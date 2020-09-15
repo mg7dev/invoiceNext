ba2715807d98ecd84e7514bdba60ca3a65c31f9b
pancy-dev <pancyboxi@gmail.com>
2020-09-16 01:37:49 +0300

    dfs

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,37 +1,44 @@
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="align-items:center;
-background-color:rgba(0, 0, 0, 0);
-box-sizing:border-box;
-color:rgb(0, 0, 0);
-cursor:pointer;
-display:flex;
-flex-direction:column;
-font-family:'Source Sans Pro', sans-serif;
-font-size:24px;
-font-weight:600;
-height:64.7569px;
-line-height:20px;
-margin-bottom:8px;
-margin-right:0px;
-overflow-wrap:break-word;
-padding-bottom:0px;
-padding-top:0px;
-text-align:left;
-text-decoration-color:rgb(0, 0, 0);
-text-decoration-line:none;
-text-decoration-style:solid;
-text-size-adjust:100%;
-white-space:nowrap;
-width:300px;
--webkit-font-smoothing:antialiased;
--webkit-tap-highlight-color:rgba(0, 0, 0, 0);">
-    <img src="http://165.232.66.234/assets/images/favicon.png" alt="InvoiceNext" style="width:25;">
+<a href="{{ $url }}" class="_mail_header_link">
+    <img src="http://165.232.66.234/assets/images/favicon.png" class="_mail_header_favicon"  alt="InvoiceNext" >
     <img src="http://165.232.66.234/assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext" style="width:50%;">
 </a>
 </td>
 </tr>
-
+<style>
+._mail_header_link{
+    align-items:center;
+    background-color:rgba(0, 0, 0, 0);
+    box-sizing:border-box;
+    color:rgb(0, 0, 0);
+    cursor:pointer;
+    display:flex;
+    flex-direction:column;
+    font-family:'Source Sans Pro', sans-serif;
+    font-size:24px;
+    font-weight:600;
+    height:64.7569px;
+    line-height:20px;
+    margin-bottom:8px;
+    margin-right:0px;
+    overflow-wrap:break-word;
+    padding-bottom:0px;
+    padding-top:0px;
+    text-align:left;
+    text-decoration-color:rgb(0, 0, 0);
+    text-decoration-line:none;
+    text-decoration-style:solid;
+    text-size-adjust:100%;
+    white-space:nowrap;
+    width:300px;
+    -webkit-font-smoothing:antialiased;
+    -webkit-tap-highlight-color:rgba(0, 0, 0, 0);
+}
+._mail_header_favicon{
+    width:25px!important;
+}
+</style>
 
 
 
