9254c642fb1cf2a5ae106c1174162c761c4aa5a6
pancy-dev <pancyboxi@gmail.com>
2020-09-16 01:29:39 +0300

    dfs

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display:flex;flex-direction:cloumn;">
-<img src="http://165.232.66.234/assets/images/favicon.png" class="logo" alt="InvoiceNext" style="width:25;">
-<img src="http://165.232.66.234/assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext" style="width:50%;">
+    <img src="http://165.232.66.234/assets/images/favicon.png" alt="InvoiceNext" style="width:25;">
+    <img src="http://165.232.66.234/assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext" style="width:50%;">
 </a>
 </td>
 </tr>
