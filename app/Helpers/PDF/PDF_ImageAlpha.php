5b50bef91142772fbcf8e26af9faee5feac8db81
pancy-dev <pancyboxi@gmail.com>
2020-09-16 18:58:45 +0300

    PDF file generation fix

--- app/Helpers/PDF/PDF_ImageAlpha.php
+++ app/Helpers/PDF/PDF_ImageAlpha.php
@@ -93,8 +93,7 @@ function Image($file, $x=null, $y=null, $w=0, $h=0, $type='', $link='', $isMask=
 // pixel-wise operation, not very fast
 function ImagePngWithAlpha($file,$x,$y,$w=0,$h=0,$link='')
 {
-    // $tmp_alpha = tempnam('/var/www/html/storage', 'mska');
-    $tmp_alpha = tempnam('.', 'mska');
+    $tmp_alpha = tempnam('/var/www/html/storage', 'mska');
     $this->tmpFiles[] = $tmp_alpha;
     $tmp_plain = tempnam('/var/www/html/storage', 'mskp');
     $this->tmpFiles[] = $tmp_plain;
