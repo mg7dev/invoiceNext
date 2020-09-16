3de7a6bf5f9b7cea25ddb13f5b92820fc9bb2f77
pancy-dev <pancyboxi@gmail.com>
2020-09-16 07:34:05 +0300

    dfs

--- vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
+++ vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
@@ -1,8 +1,45 @@
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-<img src="{{Config::get('app.url')}}assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext">
-</a>
-</td>
-</tr>
+    <td class="header">
+    <a href="{{ $url }}" class="_mail_header_link">
+        <img src="http://165.232.66.234/assets/images/favicon.png" class="_mail_header_favicon"  alt="InvoiceNext" >
+        <img src="http://165.232.66.234/assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext" style="width:30%;">
+    </a>
+    </td>
+    </tr>
+    <style>
+    ._mail_header_link{
+        width:100%!important;
+        align-items:center;
+        background-color:rgba(0, 0, 0, 0);
+        box-sizing:border-box;
+        color:rgb(0, 0, 0);
+        cursor:pointer;
+        display:flex;
+        flex-direction:column;
+        font-family:'Source Sans Pro', sans-serif;
+        font-size:24px;
+        font-weight:600;
+        height:64.7569px;
+        line-height:20px;
+        margin-bottom:8px;
+        margin-right:0px;
+        overflow-wrap:break-word;
+        padding-bottom:0px;
+        padding-top:0px;
+        text-align:left;
+        text-decoration-color:rgb(0, 0, 0);
+        text-decoration-line:none;
+        text-decoration-style:solid;
+        text-size-adjust:100%;
+        white-space:nowrap;
+        width:300px;
+        -webkit-font-smoothing:antialiased;
+        -webkit-tap-highlight-color:rgba(0, 0, 0, 0);
+    }
+    ._mail_header_favicon{
+        width:25px!important;
+    }
+    </style>
+    
+    
     
