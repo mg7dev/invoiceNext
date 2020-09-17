0f0e73d3b9ef1917fbeaa13150d537778b9ea921
pancy-dev <pancyboxi@gmail.com>
2020-09-17 12:09:15 +0300

    email sender change test

--- config/mail.php
+++ config/mail.php
@@ -39,8 +39,8 @@ return [
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' =>'victoubu@gmail.com',
+            'password' => 'tjfgiksyd',
             'timeout' => null,
         ],
 
--- resources/views/layouts/_css.blade.php
+++ resources/views/layouts/_css.blade.php
@@ -38,9 +38,6 @@
 .sidebar-menu-button ,h1.m-0{
     font-family:'Lexend Deca'
 } 
-
-
-
 .btn{
     background-color:#0fe6b0 !important;
     color:black!important;
