ae57514ef113ef81353386085b1fdd422719c0c3
pancy-dev <pancyboxi@gmail.com>
2020-09-17 20:32:01 +0300

    2020change email

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,7 @@ Route::group(['namespace' => 'CustomerPortal', 'prefix' => '/portal/{customer}',
 });
 
 // Application Routes
-Route::group(['namespace' => 'Application', 'middleware' => ['installed', 'auth' ,'dashboard','locale']], function () {
+Route::group(['namespace' => 'Application', 'middleware' => ['installed', 'auth' ,'verified' ,'dashboard','locale']], function () {
     Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 
     // Customers
