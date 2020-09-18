76ac87d047ff220544f99494f7be18bd82c5a9d5
pancy-dev <pancyboxi@gmail.com>
2020-09-18 18:03:31 +0300

    dfss

--- vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
+++ vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
@@ -1,8 +1,10 @@
 <tr>
     <td class="header">
     <a href="{{ $url }}" class="_mail_header_link">
-        <img src="http://165.232.66.234/assets/images/favicon.svg" class="logo" alt="InvoiceNext" style="width:30%;">
-        <img src="http://165.232.66.234/assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext" style="width:30%;">
+        <div style="display: table;">
+            <img src="http://165.232.66.234/assets/images/favicon.svg" class="logo" alt="InvoiceNext" style="width:30%;">
+            <img src="http://165.232.66.234/assets/images/logo-invoice-next.svg" class="logo" alt="InvoiceNext" style="width:30%;">
+        </div>
     </a>
     </td>
     </tr>
